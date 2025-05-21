3cb5ddb2eac462861fb0b8819ad25b513fcd3ed8
Alberto José <josywonder2@gmail.com>
2025-05-21 23:54:26 +0100

    feat(task): create basic starter for task controller

---
+++ src/controllers/task-controller.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Response } from 'express';
+
+import { IAuthRequest } from '../dtos/auth.dto';
+
+export const newTaskController = () => {
+  const createTask = async (
+    req: IAuthRequest,
+    res: Response,
+    next: NextFunction,
+  ) => {};
+
+  const getAllTasks = async (
+    req: IAuthRequest,
+    res: Response,
+    next: NextFunction,
+  ) => {};
+
+  const getTask = async (
+    req: IAuthRequest,
+    res: Response,
+    next: NextFunction,
+  ) => {};
+
+  const updateTask = async (
+    req: IAuthRequest,
+    res: Response,
+    next: NextFunction,
+  ) => {};
+
+  const deleteTask = async (
+    req: IAuthRequest,
+    res: Response,
+    next: NextFunction,
+  ) => {};
+
+  return {
+    createTask,
+    getAllTasks,
+    getTask,
+    updateTask,
+    deleteTask,
+  };
+};
