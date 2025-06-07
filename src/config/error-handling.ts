61cb39619092fdfa797cf90d5dd486136dcd951f
Alberto José <josywonder2@gmail.com>
2025-06-07 02:15:39 +0100

    feat: add not found error middleware

--- src/config/error-handling.ts
+++ src/config/error-handling.ts
@@ -1,7 +1,6 @@
 import { Express } from 'express';
 
 import { errorHandler } from '@/middlewares/error-handler-middleware';
-
 import { notFoundHandler } from '@/middlewares/not-found-handler';
 
 export const setupErrorHandling = (app: Express) => {
